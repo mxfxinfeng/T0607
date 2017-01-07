92fe55f3c35c09be020218f0a5390a4d756a9ff4
mxfxinfeng <mxfxinfeng@163.com>
2017-01-07 13:36:38 +0800

    更改了数据

--- src/com/mytest/github/GitDemo.java
+++ src/com/mytest/github/GitDemo.java
@@ -3,7 +3,7 @@ package com.mytest.github;
 public class GitDemo {
 
 	public static void main(String[] args) {
-		System.out.println("0607ѧϰgithub 01");
+		System.out.println("0607测试github 01");
 
 	}
 
